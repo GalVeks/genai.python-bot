f2e727bdbcaf5b256559b6f928bf562cd56f33b6
galvekselman <123359948+galvekselman@users.noreply.github.com>
2023-08-31 13:59:27 +0300

    GV: Add libreries

--- bot.py
+++ bot.py
@@ -3,13 +3,14 @@
 
 from botbuilder.core import ActivityHandler, TurnContext
 from botbuilder.schema import ChannelAccount
+from nlq import answer_my_q_ed
 
 
 class MyBot(ActivityHandler):
     # See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
 
     async def on_message_activity(self, turn_context: TurnContext):
-        await turn_context.send_activity(f"You said '{ turn_context.activity.text }'")
+        await turn_context.send_activity(answer_my_q_ed(turn_context.activity.text))
 
     async def on_members_added_activity(
         self,
---
+++ sandbox2.py
@@ -0,0 +1,4 @@
+from nlq import answer_my_q_ed
+
+
+print(answer_my_q_ed('How many artists are there?'))
