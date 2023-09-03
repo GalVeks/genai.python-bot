07a81d9492632e20da69d284c0c799afa259f31f
galvekselman <123359948+galvekselman@users.noreply.github.com>
2023-09-03 14:19:56 +0300

    GV: add credentials

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "d26c8e02-e02d-4901-9b00-435f84d6b4e2")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "yr68Q~8YsT_PYvADc3t.N4O7DFpqW-A~-xm7Vb-2")
