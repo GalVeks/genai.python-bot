6dcfcbe30ff9cd812538d4f143829ddfb5ade556
galvekselman <123359948+galvekselman@users.noreply.github.com>
2023-09-03 13:49:44 +0300

    GV: aiohttp function constraints

--- app.py
+++ app.py
@@ -80,11 +80,15 @@ async def messages(req: Request) -> Response:
         raise exception
 
 
-app = web.Application(middlewares=[aiohttp_error_middleware])
-app.router.add_post("/api/messages", messages)
+def init_func(argv):
+    APP = web.Application(middlewares=[aiohttp_error_middleware])
+    APP.router.add_post("/api/messages", messages)
+    return APP
+
 
 if __name__ == "__main__":
+    APP = init_func(None)
     try:
-        web.run_app(app, host="localhost", port=CONFIG.PORT)
+        web.run_app(APP, host="localhost", port=CONFIG.PORT)
     except Exception as error:
         raise error
